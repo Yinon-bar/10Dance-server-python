f94898f26cd98315b79201b5409d6e6b102f7843
yochaiBar <yochai456@gmail.com>
2023-08-17 00:40:25 +0300

    add secode route, get all student list

--- assets/DB/bd_conn.py
+++ assets/DB/bd_conn.py
@@ -1,19 +1,42 @@
+import pandas as pd
 import pymysql.cursors
 from pymysql import err, Error, InterfaceError
+import mysql.connector
+from mysql.connector import pooling
+from pandas import read_sql
 
 
 class DbConn:
+
+    # def __init__(self):
+    #     self.poll_conn = pooling.MySQLConnectionPool(pool_name="zerdance_pool",
+    #                                                 pool_size=2,
+    #                                                 pool_reset_session=True,
+    #                                                 host='50.87.237.60',
+    #                                                 user='zerdance_Yochai',
+    #                                                 password='Yochai@123456',
+    #                                                 database='zerdance_general')
+
     # Connect to the database
-    def get_conn(self):
+    def get_query(self,query):
         print("trying to connect...")
         try:
-            connection = pymysql.connect(host='50.87.237.60',
-                                         user='zerdance_Yochai',
-                                         password='Yochai@123456',
-                                         database='zerdance_general',
-                                         cursorclass=pymysql.cursors.DictCursor)
+            # pool_conn = pooling.MySQLConnectionPool(pool_name="zerdance_pool",
+            #                                         pool_size=2,
+            #                                         pool_reset_session=True,
+            #                                         host='50.87.237.60',
+            #                                         user='zerdance_Yochai',
+            #                                         password='Yochai@123456',
+            #                                         database='zerdance_general')
+            connection = mysql.connector.connect(host='50.87.237.60',
+                                                 user='zerdance_Yochai',
+                                                 password='Yochai@123456',
+                                                 database='zerdance_general')
             print(f"connecting succeeded. ")
-            return connection.cursor()
+            cur = connection.cursor()
+            cur.execute(query)
+            result = cur.fetchall()
+            return result
 
         except InterfaceError as e:
             print(f"got InterfaceError : {e}")
@@ -27,6 +50,8 @@ class DbConn:
         :param query: str -> plan sql query
         :return: list-> of the results
         """
-        with self.get_conn() as db:
-            db.execute(query=query)
-            return db.fetchall()
+        cur = self.poll_conn.get_connection().cursor()
+        with cur as db:
+            db.execute(query)
+            result = db.fetchall()
+            return result
--- attendance.py
+++ attendance.py
@@ -3,13 +3,14 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint,abort
 from assets.DB.bd_conn import DbConn
+import mysql.connector
 
 
 
 blp = Blueprint("attendance","attendance",description="Operations on attendance")
 
 @blp.route("/get_all")
-class Attendance(MethodView):
+class GelAllAttendance(MethodView):
 
     # def __init__(self):
     #     self.client = DbConn.get_conn()
@@ -18,8 +19,32 @@ class Attendance(MethodView):
         query = "show tables from zerdance_general "
         results =  DbConn().get_query_results(query=query)
         unpack_result = [d['Tables_in_zerdance_general'] for d in results]
+        return unpack_result
 
 
+@blp.route("/get_all_from_table/<string:table_name>")
+class GetFromTable(MethodView):
 
+    def get(self,table_name:str):
+        query = f"""select    
+                         fName,
+                        lName
+                    from {table_name}"""
 
+        # connection = mysql.connector.connect(host='50.87.237.60',
+        #                                      user='zerdance_Yochai',
+        #                                      password='Yochai@123456',
+        #                                      database='zerdance_general')
+        #
+        # mycoursor = connection.cursor()
+        #
+        # mycoursor.execute("select fName,lName from attendees")
+        #
+        # my_result = mycoursor.fetchall()
+        #
+        # print(my_result)
+
+        results = DbConn().get_query(query=query)
+        print(results)
+        return results
 
